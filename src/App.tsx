4a3fe424fbe3b9d03eea956cbb5da365a1051116
nyx <nyx.webapp@gmail.com>
2022-06-19 23:08:39 +0800

    working inject

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import ExcalDraw from "./ExcalDraw";
+
+function App() {
+  return (
+    <div className="App">
+      <ExcalDraw />
+    </div>
+  );
+}
+
+export default App;
